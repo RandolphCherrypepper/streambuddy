329b6de5078d39a077b3d4162a1b844f39e3a0d9
Bryan Bonvallet <btbonval@gmail.com>
2024-01-06 18:11:05 -0800

    Adding environment examples

---
+++ exampleenv.bat
@@ -0,0 +1,5 @@
+set TWITCH_AUTH_CLIENTID=<redacted>
+set TWITCH_AUTH_SECRET=<redacted>
+set TWITCH_AUTH_DEVICECODE=<redacted>
+set TWITCH_AUTH_SCOPES="channel:bot channel:moderate chat:edit chat:read user:bot user:read:chat whispers:read whispers:edit"
+set OPENAI_API_KEY=<redacted>
